90e9b83633bf0e7d98724c6a268fddab896b2dac
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-09-17 17:28:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ resource "google_compute_instance" "terraform" {
   project      = "secret-proton-312811"
   name         = "terraform"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"  boot_disk {
+  zone         = "us-central1-a"  
+  
+  boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
     }
-  }  network_interface {
+  }  
+  
+  network_interface {
     network = "default"
     access_config {
     }
