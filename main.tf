0abbf1df3f43bb7a18cf1d3d18cd621fafccfd9d
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-09-17 19:01:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/home/rajeevgcp14/key2.json")
+  credentials = file("secret-proton-312811-ae1e8cfe87ed.json")
 }
 
 resource "google_compute_instance" "terraform" {
